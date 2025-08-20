f0cb82f30762f5779dd6519ae7e95bef57da772e
212739 <michael.mckenzie@spartannash.com>
2025-08-20 15:32:43 -0400

    Added virtual environment, tested connectivity locally to snowflake. Pushing to test get_sesion in service

--- Home.py
+++ Home.py
@@ -1,5 +1,8 @@
 from utils.session import get_session
+import streamlit as st
 
 session = get_session()
 
 temp_df = session.table('EDW.RTL.RETAIL_SALES').limit(10).to_pandas()
+
+st.write(temp_df)
