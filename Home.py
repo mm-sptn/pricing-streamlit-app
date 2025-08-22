c0b115bd3646749c1a31d0b1cf5c834e507ef064
212739 <michael.mckenzie@spartannash.com>
2025-08-22 14:04:11 -0400

    changed app layout and nav functionality

--- Home.py
+++ Home.py
@@ -1,11 +1,3 @@
-from utils.session import get_cached_session
-import utils.get_data as gd
 import streamlit as st
 
-session = get_cached_session()
-
-temp_df = session.table('EDW.RTL.RETAIL_SALES').limit(10).to_pandas()
-
-df = gd.get_price_strategies()
-
-st.write(df)
+st.title("Welcome to the Pricing App")
---
+++ streamlit_app.py
@@ -0,0 +1,21 @@
+from utils.session import get_cached_session
+import utils.get_data as gd
+import streamlit as st
+
+session = get_cached_session()
+
+df = gd.get_price_strategies()
+
+# Define pages
+home = st.Page("Home.py", title="🏠 Home")
+data_explorer = st.Page("Data Explorer.py", title="📊 Data Explorer")
+pz_consolidation = st.Page("Price Zone Consolidation.py", title="💲 Price Zone Consolidation")
+
+# Navigation
+pg = st.navigation([home, data_explorer, pz_consolidation])
+
+# App config
+st.set_page_config(page_title="Pricing App", page_icon=":moneybag:", layout="wide")
+
+# Run nav
+pg.run()
